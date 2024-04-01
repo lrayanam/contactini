a81d0535129f9993340c9eb4933023f7d58846fd
contactini <rayan@contactini.fr>
2024-04-01 23:50:01 +0200

    add mongoose and connect with my local mongodb

---
+++ contactini-backend/db/mongo.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+exports.initClientDbConnection = async () => {
+    try {
+        let url = 'mongodb://'+process.env.USERDB+':'+process.env.PWDDB+'@localhost:27017/'+process.env.DBNAME
+        await mongoose.connect(url);        
+    //    await mongoose.connect('mongodb://localhost:27017/'+process.env.DBNAME)
+        console.log('Connected');
+
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
